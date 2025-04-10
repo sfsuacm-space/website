39b9f6aba9637c3bfb1541f137017d89bf559bbb
Huy Dang <ghuydang@gmail.com>
2025-04-09 21:56:19 -0700

    change labels from abbreviations to full org. names

--- src/constants/organizations.ts
+++ src/constants/organizations.ts
@@ -4,50 +4,50 @@ import { AffiliateOrganization } from "@/types/affiliateOrganization";
 const affiliateOrganizations: AffiliateOrganization[] = [
   {
     fullName: "3D Printing",
-    name: "3D Printing",      
+    name: "3D Printing",
     joinLink: "https://discord.gg/UcRnnMVKfW",
   },
   {
     fullName: "Spark SF Club",
-    name: "Spark SF",      
+    name: "Spark SF",
     joinLink: "https://discord.gg/XCwRXspP82",
   },
-    { 
+  {
     fullName: "Artitficial Intelligence Club",
     name: "AI",
     joinLink: "https://discord.gg/7C8729PAjA",
-    },
-    { 
+  },
+  {
     fullName: "Cybersecurity Club",
     name: "Cybersecurity",
     joinLink: "https://discord.gg/fsJ86BmdW6",
-    },
-     { 
+  },
+  {
     fullName: "Bit-Bots Club",
     name: "Bit-Bots",
     joinLink: "https://discord.gg/eZFAtfpRxR",
-    },
-    { 
+  },
+  {
     fullName: "ColorStack Student Chapter at SFSU",
     name: "ColorStack",
     joinLink: "https://discord.gg/zBkjqqdFDu",
-    },
-     { 
+  },
+  {
     fullName: "Game Development Club",
     name: "Game Development",
     joinLink: "https://discord.gg/yqb4ZXUvxB",
-    },
-     { 
-    fullName: "Society of Women's Engineers Student Chapter at SFSU",
-    name: "SWE",
+  },
+  {
+    fullName: "Society of Women Engineers Student Chapter at SFSU",
+    name: "Society of Women Engineers",
     joinLink: "https://discord.gg/ZhGQ5ZBZWY",
-    },
-    { 
+  },
+  {
     fullName: "Data Science Society Club",
-    name: "DSS",
+    name: "Data Science Society",
     joinLink: "https://discord.gg/GUQ6vWAxt3",
-    },
-  ]
+  },
+]
 
 
 export default affiliateOrganizations;
